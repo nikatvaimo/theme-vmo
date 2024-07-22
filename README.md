52a553015a07bf5b0782fbd4412f7f24dd42ed0e
Nikita Merežko <nikita.merezhko@vaimo.com>
2024-07-22 12:42:28 +0300

    init theme

---
+++ README.md
@@ -0,0 +1,16 @@
+# vaimo/theme-vmo
+
+Theme module for the vmo project
+
+## Theme strcuture
+
+Theme is a child to Magento/blank
+
+
+## Workflow
+
+TBD
+
+```sh
+$ m2 dev:source-theme:deploy --theme="Vaimo/vmo" --locale="en_US" --area="frontend"
+```
---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2009-2024 Vaimo Group. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Vaimo/vmo',
+    __DIR__
+);
